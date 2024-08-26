4aa23e949507c8507d6228a1633084ee03f668dd
Subx1s0o <nazarsabardin@gmail.com>
2024-08-26 03:16:12 +0300

    fix: remove text styles from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,48 +20,6 @@ const config: Config = {
         black: "#2B2423",
         white: "#FAF8F0",
       },
-      textStyles: {
-        mobileH1: {
-          fontSize: "36px",
-          fontWeight: "600",
-          color: "#000000",
-          lineHeight: "20px",
-        },
-        mobileH2: {
-          fontSize: "32px",
-          fontWeight: "500",
-          color: "#000000",
-          lineHeight: "20px",
-        },
-        mobileH3: {
-          fontSize: "20px",
-          fontWeight: "500",
-          color: "#000000",
-          lineHeight: "20px",
-        },
-        mobileButton: {
-          fontSize: "20px",
-          fontWeight: "500",
-          lineHeight: "20px",
-        },
-
-        MobileMenuLink: {
-          fontSize: "20px",
-          fontWeight: "400",
-          lineHeight: "20px",
-        },
-        mobileRegular: {
-          fontSize: "14px",
-          fontWeight: "400",
-          lineHeight: "20px",
-        },
-        mobileCaptions: {
-          fontSize: "13px",
-          color: "#000000",
-          fontWeight: "400",
-          lineHeight: "20px",
-        },
-      },
     },
   },
   plugins: [],
