e48f1262e4c4cd1481a4ad992dc4e04b00238df3
Subx1s0o <nazarsabardin@gmail.com>
2024-09-01 01:34:31 +0300

    feat: add robots

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      disallow: ["/services/makeup", "/services/style"],
+      allow: ["/portfolio", "/about-me", "/services", "/contacts"],
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/sitemap.xml`,
+  };
+}
