5304586429350b98a6ab129d98187f13d63dbaab
Subx1s0o <nazarsabardin@gmail.com>
2024-09-11 01:30:04 +0300

    fix: little changes

--- src/components/sections/home/PortfolioSection.tsx
+++ src/components/sections/home/PortfolioSection.tsx
@@ -1,6 +1,34 @@
 import CustomLink from "@/components/ui/CustomLink";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+
+const images = [
+  {
+    src: "/images/portfolio/portfolio.avif",
+    width: 168,
+    height: 214,
+    span: "row-span-2",
+  },
+  {
+    src: "/images/portfolio/portfolio2.avif",
+    width: 167,
+    height: 258,
+    span: "row-span-3",
+  },
+  {
+    src: "/images/portfolio/portfolio3.avif",
+    width: 168,
+    height: 268,
+    span: "row-span-3",
+  },
+  {
+    src: "/images/portfolio/portfolio4.avif",
+    width: 168,
+    height: 224,
+    span: "row-span-2",
+  },
+];
+
 export default function PortfolioSection() {
   const t = useTranslations("Home.portfolio");
   return (
@@ -8,59 +36,20 @@ export default function PortfolioSection() {
       <div className="my-container">
         <h2 className="text-md font-exo2 text-center mb-5">{t("title")}</h2>
         <p className="mb-3 text-sm">{t("description")}</p>
-        <ul className="grid grid-cols-2 grid-rows-2 gap-2 mb-3 ">
-          <li className=" row-span-2">
-            <Image
-              width={168}
-              height={214}
-              style={{
-                width: "100%",
-                height: "100%",
-          
-              }}
-              src="/images/portfolio/portfolio.avif"
-              alt="portfolio image"
-            />
-          </li>
-          <li className=" row-span-3">
-            <Image
-              width={167}
-              height={258}
-              style={{
-                width: "100%",
-                height: "auto",
-          
-              }}
-              src="/images/portfolio/portfolio2.avif"
-              alt="portfolio image"
-            />
-          </li>
-          <li className=" row-span-3">
-            <Image
-              width={168}
-              height={268}
-              style={{
-                width: "100%",
-                height: "auto",
-          
-              }}
-              src="/images/portfolio/portfolio3.avif"
-              alt="portfolio image"
-            />
-          </li>
-          <li className=" row-span-2">
-            <Image
-              width={168}
-              height={224}
-              style={{
-                width: "100%",
-                height: "auto",
-               
-              }}
-              src="/images/portfolio/portfolio4.avif"
-              alt="portfolio image"
-            />
-          </li>
+        <ul className="grid grid-cols-2 grid-rows-2 gap-2 mb-3">
+          {images.map((img, index) => (
+            <li key={index} className={img.span}>
+              <Image
+                width={img.width}
+                height={img.height}
+                style={{ width: "100%", height: "auto" }}
+                src={img.src}
+                alt={`portfolio image ${index + 1}`}
+                layout="responsive"
+                priority
+              />
+            </li>
+          ))}
         </ul>
         <CustomLink variant="outline" href="/portfolio">
           {t("see-more")}
